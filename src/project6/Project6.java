f5bb80b3177d355c48786ce409cadbe9c224969c
mohammed-kh20 <mohammedjawadkhader@gmail.com>
2025-04-07 15:44:10 +0200

    Project6.java

--- src/project6/Project6.java
+++ src/project6/Project6.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.time.LocalDate;
 public class Project6 { 
     public static void main(String[] args) {
-        //Hello Guys
+       
         Scanner sc=new Scanner(System.in);
         MainProgram mainprogram=new MainProgram();
         ArrayList<String>infoFromFile=mainprogram.readFile("C:\\Users\\LENOVO\\OneDrive - Arab American University\\سطح المكتب\\Extra\\javapro2\\eDevices.txt");
